0783f23379bf4c1e784deb01bdd24da6ae9d6718
Pat <reminiz@gmail.com>
2019-11-20 16:41:29 +0700

    Fix

--- app/app.js
+++ app/app.js
@@ -20,10 +20,10 @@ function changebgcolor(){
 var body = document.getElementsByTagName('body')[0]
 
 
-window.addEventListener("touchstart", function(evt){
-	console.log('touch')
-	changebgcolor()
-}, false);
+// window.addEventListener("touchstart", function(evt){
+// 	console.log('touch')
+// 	changebgcolor()
+// }, false);
 
 body.onclick = function(){
 	console.log('click')
